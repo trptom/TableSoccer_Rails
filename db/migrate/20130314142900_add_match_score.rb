729362fa4101098bb7f696ccfab139ce51b4656e
Tomas Praslicak <tpraslicak@seznam.cz>
2013-03-22 14:46:54 +0100

    Opravy pro deploy, oprava lokalizace

--- db/migrate/20130314142900_add_match_score.rb
+++ db/migrate/20130314142900_add_match_score.rb
@@ -1,8 +1,12 @@
 class AddMatchScore < ActiveRecord::Migration
-  def change
+  def self.up
     change_table :matches do |t|
       t.integer :score_home,  :null => true, :default => nil
       t.integer :score_away,  :null => true, :default => nil
     end
+  end
+
+  def self.down
+    
   end
 end
