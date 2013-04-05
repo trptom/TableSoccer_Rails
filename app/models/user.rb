f7ec19d89132f40a4e6faf4f73388ba74c8998ee
Tomas Praslicak <tpraslicak@seznam.cz>
2013-04-05 13:07:40 +0200

    Oprava editace uživatele
    Už lze přidat i nulového propojeného hráče.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,15 @@ class User < ActiveRecord::Base
 
   validates :username,
     :length => { :minimum => 3, :maximum => 255, :message => "špatná délka uživatelského jména (3-255)" },
-    :uniqueness => { :case_sensitive => false, :message => "uživatel s daným jménem již existuje" },
-  :if => :username
+    :uniqueness => { :case_sensitive => false, :message => "uživatel s daným jménem již existuje" }
 
   validates :email,
     :format => { :with => /^.+@.+\..+$/, :message => "chybný formát emailu" },
     :allow_blank => true,
-    :allow_nil => true,
-  :if => :email
+    :allow_nil => true
 
-  validates :player_id, :presence => { :message => "chybný propojený hráč" }
-  validates :player, :associated => { :message => "chybný propojený hráč" }
+  validates :player_id, :presence => { :message => "chybný propojený hráč" }, :allow_blank => true, :allow_nil => true, :if => :player_id
+  validates :player, :associated => { :message => "chybný propojený hráč" }, :allow_blank => true, :allow_nil => true
 
   validates_length_of :password, :minimum => 3, :message => "heslo musí mít alespoň 3 znaky", :if => :password
   validates_confirmation_of :password, :message => "heslo a jeho kontrola se liší", :if => :password
