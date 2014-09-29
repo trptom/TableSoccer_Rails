c9fe18ba9ddec8e45c72d03ffced29ece0d71f5c
Tomas Praslicak <tpraslicak@seznam.cz>
2014-09-29 15:09:34 +0200

    Uprava lig, aby to bylo +- ok (krafika, vse do messages, ...). Odstraneni minipocitadla pristupu. Trusku jsem hrabnul i do uzivatel, ale nic zasadniho (jen jsem si to poplet s ligou, tak uz jsem to tam nechal, kdyz bych to pak stejne delal :-) )

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -2,18 +2,15 @@ class LeaguesController < ApplicationController
   before_filter :require_login, except: [:show]
   
   # GET /leagues
-  # GET /leagues.json
   def index
     @leagues = League.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @leagues }
     end
   end
 
   # GET /leagues/1
-  # GET /leagues/1.json
   def show
     @league = League.find(params[:id])
     @teams_list = @league.league_teams.order(:season).all
@@ -34,67 +31,79 @@ class LeaguesController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @league }
     end
   end
 
   # GET /leagues/new
-  # GET /leagues/new.json
   def new
     @league = League.new
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @league }
+      format.html {
+        @buttons = [
+          { :body => I18n.t("messages.leagues.form.back_to_list"), :url => leagues_path, :html_options => {} }
+        ]
+      }
     end
   end
 
   # GET /leagues/1/edit
   def edit
     @league = League.find(params[:id])
+    
+    respond_to do |format|
+      format.html {
+        @buttons = [
+          { :body => I18n.t("messages.leagues.edit.back_to_detail"), :url => @league , :html_options => {} },
+          { :body => I18n.t("messages.leagues.form.back_to_list"), :url => leagues_path, :html_options => {} }
+        ]
+      }
+    end
   end
 
   # POST /leagues
-  # POST /leagues.json
   def create
     @league = League.new(params[:league])
 
     respond_to do |format|
       if @league.save
-        format.html { redirect_to @league, notice: 'League was successfully created.' }
-        format.json { render json: @league, status: :created, location: @league }
+        format.html {
+          redirect_to @league, notice: I18n.t("messages.leagues.create.success")
+        }
       else
-        format.html { render action: "new" }
-        format.json { render json: @league.errors, status: :unprocessable_entity }
+        format.html {
+          render action: "new"
+        }
       end
     end
   end
 
   # PUT /leagues/1
-  # PUT /leagues/1.json
   def update
     @league = League.find(params[:id])
 
     respond_to do |format|
       if @league.update_attributes(params[:league])
-        format.html { redirect_to @league, notice: 'League was successfully updated.' }
-        format.json { head :no_content }
+        format.html {
+          redirect_to @league, notice: I18n.t("messages.leagues.update.success")
+        }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @league.errors, status: :unprocessable_entity }
+        format.html {
+          render action: "edit"
+        }
       end
     end
   end
 
   # DELETE /leagues/1
-  # DELETE /leagues/1.json
   def destroy
     @league = League.find(params[:id])
     @league.destroy
 
     respond_to do |format|
-      format.html { redirect_to leagues_url }
-      format.json { head :no_content }
+      format.html {
+        redirect_to leagues_url
+      }
     end
   end
 end
