b6467f25e94c5180bf95259ef5c37ac9ff636d58
trptom <prasltom@fel.cvut.cz>
2014-10-01 22:09:43 +0200

    Debug hlasky do logu pro login.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,9 +1,12 @@
+# coding:utf-8
 class UserSessionsController < ApplicationController
   before_filter :require_login, only: [:destroy]
   
 #  skip_before_filter :check_permissions, :except => [:destroy]
   def create
     respond_to do |format|
+	  Rails.logger.info "un: " + params[:username]
+	  Rails.logger.info "ps: " + params[:password]
       if @user = login(params[:username],params[:password])
         format.html {
           redirect_back_or_to("/", :notice => I18n.t("messages.user_sessions.create.succesfull"))
