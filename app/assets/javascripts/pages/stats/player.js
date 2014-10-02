9eeb2f86c813920d3fcbfbea9c52853d31c8945e
Tomas Praslicak <tpraslicak@seznam.cz>
2014-10-02 11:39:26 +0200

    Jeste script co mi predtim utek.

--- app/assets/javascripts/pages/stats/player.js
+++ app/assets/javascripts/pages/stats/player.js
@@ -1,6 +1,6 @@
 function changePlayer() {
     var season = $("#season_select").val();
-    alert(season);
+    
     if (season && parseInt(season, 10) > 0) {
         redirect("/stats/player/" + $("#player_select").val() + "/" + season);
     } else {
