785c674b2995857eac1a9849cc24f764f3da7589
Tomas Praslicak <tpraslicak@seznam.cz>
2014-10-02 08:52:21 +0200

    Pridani routy na aktivaci.

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,11 @@ TableSoccer::Application.routes.draw do
 
   resources :user_sessions
 
-  resources :users
+  resources :users do
+    member do
+      get :activate
+    end
+  end
 
   match 'matches/:id/add_game' => 'matches#add_game'
   match 'players/:id/stats' => 'stats#player'
