e607883fc18a4001b5261ae91eb6eda6e8577eee
Tomas Praslicak <tpraslicak@seznam.cz>
2014-10-08 21:16:39 +0200

    Uprava menu pro prihlaseneho uzivatele (pridani doplnovani dochazky).

--- db/schema.rb
+++ db/schema.rb
@@ -108,9 +108,9 @@ ActiveRecord::Schema.define(:version => 20141005164032) do
   add_index "possible_date_selections", ["possible_date_id"], :name => "index_possible_date_selections_on_possible_date_id"
 
   create_table "possible_dates", :force => true do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer  "match_id"
+    t.datetime "start_time", :null => false
+    t.datetime "end_time",   :null => false
+    t.integer  "match_id",   :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
