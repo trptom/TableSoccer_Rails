78f2372ee37a9d9c275f8961df08bc62d086487e
Tomas Praslicak <tpraslicak@seznam.cz>
2014-10-20 14:28:30 +0200

    Razeni statistik hrace.

--- app/assets/javascripts/global/global.js
+++ app/assets/javascripts/global/global.js
@@ -55,3 +55,127 @@ function dateFromDb(dbDateStr) {
             parseInt(tmpHMS[2], 10),
             0));
 }
+
+/**
+ * 
+ * @param {HTMLElement} sender header cell that sent sort request.
+ * @param {String} tableId id of table to be sorted.
+ * @param {Number} colId 0-based number of column by which should be table
+ * sorted.
+ * @param {function(Object, Object)} comparator function that accepts two
+ * objects (innerHTML of cells) to be compared and returns number (&lt; 0 when
+ * o1 &lt; o2, 0 when o1 == o2 end &gt; 0 when o1 &gt; o2). When null, cells are
+ * compared by its innerHTML (o1.innerHTML &lt; o2.innerHTML).
+ * @param {function(HTMLElement)} selector function that accepts oen parameter
+ * (table row) and decides whether this row should be displayed (returns
+ * true/false). When null, all rows are accepted.
+ * @returns {undefined}
+ */
+function sortTable(sender, tableId, colId, comparator, selector) {
+    comparator = comparator ? comparator : Comparator.defaultComparator;
+    
+    // check whether should be sorted ASC or DESC
+    var type = 1; // ASC
+    if ($(sender).data("sort-type") == "DESC") {
+        type = -1;
+    }
+    $(sender).data("sort-type", type === 1 ? "DESC" : "ASC"); // set to opposite for next click
+    
+    // get table body
+    var tableBody = document.getElementById(tableId).getElementsByTagName("tbody")[0];
+    
+    // get list of rows
+    var rowsSelect = tableBody.getElementsByTagName("tr");
+    var rows = [];
+    for (var a=0; a<rowsSelect.length; a++) {
+        rows[rows.length] = rowsSelect[a];
+    }
+    
+    
+    // get list of cells
+    var cells = [];
+    for (var a=0; a<rows.length; a++) {
+        cells[a] = rows[a].getElementsByTagName("td")[colId];
+        console.log("cells[" + a + "] = " + cells[a].innerHTML);
+    }
+    
+    // sort all rows/cells
+    for (var a=0; a<rows.length-1; a++) {
+        for (var b=a; b>=0; b--) {
+            if (comparator(cells[b].innerHTML, cells[b+1].innerHTML) * type > 0) {
+                var tmp = rows[b];
+                rows[b] = rows[b+1];
+                rows[b+1] = tmp;
+                var tmp = cells[b];
+                cells[b] = cells[b+1];
+                cells[b+1] = tmp;
+            }
+        }
+    }
+    
+    // show/hide rows depending on selector
+    for (var a=0; a<rows.length; a++) {
+        if (!selector || selector(this)) {
+            $(rows[a]).removeClass("non-displayed");
+        } else {
+            $(rows[a]).addClass("non-displayed");
+        }
+    }
+    
+    // remove all rows from table
+    for (var a=0; a<rows.length; a++) {
+        tableBody.removeChild(rows[a]);
+        console.log("deleting " + a);
+    }
+    
+    // add all rows in right order
+    for (var a=0; a<rows.length; a++) {
+        tableBody.appendChild(rows[a]);
+        console.log("adding " + a);
+    }
+    
+    if (type === 1) {
+        $(sortTable.arrow.asc).removeClass("non-displayed");
+        $(sortTable.arrow.desc).addClass("non-displayed");
+        sender.appendChild(sortTable.arrow.asc);
+    } else {
+        $(sortTable.arrow.desc).removeClass("non-displayed");
+        $(sortTable.arrow.asc).addClass("non-displayed");
+        sender.appendChild(sortTable.arrow.desc);
+    }
+}
+
+sortTable.arrow = {
+    asc: document.createElement("span"),
+    desc: document.createElement("span")
+};
+
+sortTable.arrow.asc.innerHTML = "&nbsp;";
+sortTable.arrow.desc.innerHTML = "&nbsp;";
+$(sortTable.arrow.asc).addClass("icon icon-circle-arrow-up non-displayed");
+$(sortTable.arrow.desc).addClass("icon icon-circle-arrow-down non-displayed");
+
+Comparator = {
+    defaultComparator: function(o1, o2) {
+        if (o1 > o2) {
+            return 1;
+        }
+        if (o1 < o2) {
+            return -1;
+        }
+        return 0;
+    },
+    
+    percentage: function(o1, o2) {
+        var o1d = parseFloat(o1.replace(/ +%$/, ""));
+        var o2d = parseFloat(o1.replace(/ +%$/, ""));
+
+        if (o1d > o2d) {
+            return 1;
+        }
+        if (o1d < o2d) {
+            return -1;
+        }
+        return 0;
+    }
+};
--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -2,25 +2,27 @@ module StatsHelper
   def generate_player_stats_model
     stat_types = [:gp, :w, :t, :l, :gf, :ga, :percentage, :rate]
 
-    stats = Array.new
-    stats[GAME_TYPE_SINGLE] = {}
-    stats[GAME_TYPE_DOUBLE] = {}
-    stats[GAME_TYPE_FOURS] = {}
-    stats[GAME_TYPE_TWO_BALL] = {}
-    stats[GAME_TYPE_STR.length] = {}
+    stats = {
+      :gt => [],
+      :total => {}
+    }
+    stats[:gt] = Array.new
+    stats[:gt][GAME_TYPE_SINGLE] = {}
+    stats[:gt][GAME_TYPE_DOUBLE] = {}
+    stats[:gt][GAME_TYPE_FOURS] = {}
+    stats[:gt][GAME_TYPE_TWO_BALL] = {}
 
-    stats[GAME_TYPE_SINGLE][:game_type] = GAME_TYPE_SINGLE
-    stats[GAME_TYPE_DOUBLE][:game_type] = GAME_TYPE_DOUBLE
-    stats[GAME_TYPE_FOURS][:game_type] = GAME_TYPE_FOURS
-    stats[GAME_TYPE_TWO_BALL][:game_type] = GAME_TYPE_TWO_BALL
-    stats[GAME_TYPE_STR.length][:game_type] = GAME_TYPE_STR.length
+    stats[:gt][GAME_TYPE_SINGLE][:game_type] = GAME_TYPE_SINGLE
+    stats[:gt][GAME_TYPE_DOUBLE][:game_type] = GAME_TYPE_DOUBLE
+    stats[:gt][GAME_TYPE_FOURS][:game_type] = GAME_TYPE_FOURS
+    stats[:gt][GAME_TYPE_TWO_BALL][:game_type] = GAME_TYPE_TWO_BALL
 
     stat_types.each{|a|
-      stats[GAME_TYPE_SINGLE][a] = 0
-      stats[GAME_TYPE_DOUBLE][a] = 0
-      stats[GAME_TYPE_FOURS][a] = 0
-      stats[GAME_TYPE_TWO_BALL][a] = 0
-      stats[GAME_TYPE_STR.length][a] = 0
+      stats[:gt][GAME_TYPE_SINGLE][a] = 0
+      stats[:gt][GAME_TYPE_DOUBLE][a] = 0
+      stats[:gt][GAME_TYPE_FOURS][a] = 0
+      stats[:gt][GAME_TYPE_TWO_BALL][a] = 0
+      stats[:total][a] = 0
     }
 
     return stats
@@ -35,59 +37,59 @@ module StatsHelper
     
     for gp in @source
       if (gp.game && gp.game.score_home && gp.game.score_away)
-        stats[GAME_TYPE_STR.length][:gp] += 1;
-        stats[gp.game.game_type][:gp] += 1;
+        stats[:total][:gp] += 1;
+        stats[:gt][gp.game.game_type][:gp] += 1;
         diff = gp.game.score_home - gp.game.score_away
         if gp.team == TEAM_HOME
-          stats[GAME_TYPE_STR.length][:gf] += gp.game.score_home;
-          stats[GAME_TYPE_STR.length][:ga] += gp.game.score_away;
-          stats[gp.game.game_type][:gf] += gp.game.score_home;
-          stats[gp.game.game_type][:ga] += gp.game.score_away;
+          stats[:total][:gf] += gp.game.score_home;
+          stats[:total][:ga] += gp.game.score_away;
+          stats[:gt][gp.game.game_type][:gf] += gp.game.score_home;
+          stats[:gt][gp.game.game_type][:ga] += gp.game.score_away;
           if diff > 0
-            stats[GAME_TYPE_STR.length][:w] += 1
-            stats[gp.game.game_type][:w] += 1
+            stats[:total][:w] += 1
+            stats[:gt][gp.game.game_type][:w] += 1
           end
           if diff == 0
-            stats[GAME_TYPE_STR.length][:t] += 1
-            stats[gp.game.game_type][:t] += 1
+            stats[:total][:t] += 1
+            stats[:gt][gp.game.game_type][:t] += 1
           end
           if diff < 0
-            stats[GAME_TYPE_STR.length][:l] += 1
-            stats[gp.game.game_type][:l] += 1
+            stats[:total][:l] += 1
+            stats[:gt][gp.game.game_type][:l] += 1
           end
         end
         if gp.team == TEAM_AWAY
-          stats[GAME_TYPE_STR.length][:ga] += gp.game.score_home;
-          stats[GAME_TYPE_STR.length][:gf] += gp.game.score_away;
-          stats[gp.game.game_type][:ga] += gp.game.score_home;
-          stats[gp.game.game_type][:gf] += gp.game.score_away;
+          stats[:total][:ga] += gp.game.score_home;
+          stats[:total][:gf] += gp.game.score_away;
+          stats[:gt][gp.game.game_type][:ga] += gp.game.score_home;
+          stats[:gt][gp.game.game_type][:gf] += gp.game.score_away;
           if diff < 0
-            stats[GAME_TYPE_STR.length][:w] += 1
-            stats[gp.game.game_type][:w] += 1
+            stats[:total][:w] += 1
+            stats[:gt][gp.game.game_type][:w] += 1
           end
           if diff == 0
-            stats[GAME_TYPE_STR.length][:t] += 1
-            stats[gp.game.game_type][:t] += 1
+            stats[:total][:t] += 1
+            stats[:gt][gp.game.game_type][:t] += 1
           end
           if diff > 0
-            stats[GAME_TYPE_STR.length][:l] += 1
-            stats[gp.game.game_type][:l] += 1
+            stats[:total][:l] += 1
+            stats[:gt][gp.game.game_type][:l] += 1
           end
         end
       end
     end
 
-    stats[GAME_TYPE_STR.length][:rate] = stats[GAME_TYPE_STR.length][:gp] > 0 ? stats[GAME_TYPE_STR.length][:w].to_f/stats[GAME_TYPE_STR.length][:gp].to_f : 0;
-    stats[GAME_TYPE_SINGLE][:rate] = stats[GAME_TYPE_SINGLE][:gp] > 0 ? stats[GAME_TYPE_SINGLE][:w].to_f/stats[GAME_TYPE_SINGLE][:gp].to_f : 0;
-    stats[GAME_TYPE_DOUBLE][:rate] = stats[GAME_TYPE_DOUBLE][:gp] > 0 ? stats[GAME_TYPE_DOUBLE][:w].to_f/stats[GAME_TYPE_DOUBLE][:gp].to_f : 0;
-    stats[GAME_TYPE_FOURS][:rate] = stats[GAME_TYPE_FOURS][:gp] > 0 ? stats[GAME_TYPE_FOURS][:w].to_f/stats[GAME_TYPE_FOURS][:gp].to_f : 0;
-    stats[GAME_TYPE_TWO_BALL][:rate] = stats[GAME_TYPE_TWO_BALL][:gp] > 0 ? stats[GAME_TYPE_TWO_BALL][:w].to_f/stats[GAME_TYPE_TWO_BALL][:gp].to_f : 0;
+    stats[:total][:rate] = stats[:total][:gp] > 0 ? stats[:total][:w].to_f/stats[:total][:gp].to_f : 0;
+    stats[:gt][GAME_TYPE_SINGLE][:rate] = stats[:gt][GAME_TYPE_SINGLE][:gp] > 0 ? stats[:gt][GAME_TYPE_SINGLE][:w].to_f/stats[:gt][GAME_TYPE_SINGLE][:gp].to_f : 0;
+    stats[:gt][GAME_TYPE_DOUBLE][:rate] = stats[:gt][GAME_TYPE_DOUBLE][:gp] > 0 ? stats[:gt][GAME_TYPE_DOUBLE][:w].to_f/stats[:gt][GAME_TYPE_DOUBLE][:gp].to_f : 0;
+    stats[:gt][GAME_TYPE_FOURS][:rate] = stats[:gt][GAME_TYPE_FOURS][:gp] > 0 ? stats[:gt][GAME_TYPE_FOURS][:w].to_f/stats[:gt][GAME_TYPE_FOURS][:gp].to_f : 0;
+    stats[:gt][GAME_TYPE_TWO_BALL][:rate] = stats[:gt][GAME_TYPE_TWO_BALL][:gp] > 0 ? stats[:gt][GAME_TYPE_TWO_BALL][:w].to_f/stats[:gt][GAME_TYPE_TWO_BALL][:gp].to_f : 0;
 
-    stats[GAME_TYPE_STR.length][:percentage] = sprintf('%.2f', stats[GAME_TYPE_STR.length][:rate]*100)
-    stats[GAME_TYPE_SINGLE][:percentage] = sprintf('%.2f', stats[GAME_TYPE_SINGLE][:rate]*100)
-    stats[GAME_TYPE_DOUBLE][:percentage] = sprintf('%.2f', stats[GAME_TYPE_DOUBLE][:rate]*100)
-    stats[GAME_TYPE_FOURS][:percentage] = sprintf('%.2f', stats[GAME_TYPE_FOURS][:rate]*100)
-    stats[GAME_TYPE_TWO_BALL][:percentage] = sprintf('%.2f', stats[GAME_TYPE_TWO_BALL][:rate]*100)
+    stats[:total][:percentage] = sprintf('%.2f', stats[:total][:rate]*100)
+    stats[:gt][GAME_TYPE_SINGLE][:percentage] = sprintf('%.2f', stats[:gt][GAME_TYPE_SINGLE][:rate]*100)
+    stats[:gt][GAME_TYPE_DOUBLE][:percentage] = sprintf('%.2f', stats[:gt][GAME_TYPE_DOUBLE][:rate]*100)
+    stats[:gt][GAME_TYPE_FOURS][:percentage] = sprintf('%.2f', stats[:gt][GAME_TYPE_FOURS][:rate]*100)
+    stats[:gt][GAME_TYPE_TWO_BALL][:percentage] = sprintf('%.2f', stats[:gt][GAME_TYPE_TWO_BALL][:rate]*100)
 
     return stats
   end
